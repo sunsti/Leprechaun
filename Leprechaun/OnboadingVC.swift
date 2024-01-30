43fffa66b45b15f814013b02eb6c09da26be7923
Dmitriy Holovnia <dmitriy.holovnia@uptech.team>
2024-01-30 22:01:05 +0200

    update onboarding

--- Leprechaun/OnboadingVC.swift
+++ Leprechaun/OnboadingVC.swift
@@ -27,16 +27,22 @@ class OnboadingVC: UIViewController {
         Task {
             do {
                 try await ManagerNetwork.shared.authenticate()
-                let vc = HomeVC()
-                let navigationController = UINavigationController(rootViewController: vc)
-                navigationController.modalPresentationStyle = .fullScreen
-                present(navigationController, animated: true)
-                navigationController.setNavigationBarHidden(true, animated: false)
+                presentHomeScreen()
             } catch {
                 print("Error: \(error.localizedDescription)")
+                presentHomeScreen()
             }
         }
     }
+    
+    func presentHomeScreen() {
+        let vc = HomeVC()
+        let navigationController = UINavigationController(rootViewController: vc)
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true)
+        navigationController.setNavigationBarHidden(true, animated: false)
+    }
+    
     func animateProgressBar() {
         UIView.animate(withDuration: 1.5) {
             // Установите конечное значение прогресса
